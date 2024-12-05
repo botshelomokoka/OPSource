c3a72f6644ce051ba61e141dead5339e9f5d1c49
botshelomokoka <botshelomokoka@gmail.com>
2024-12-05 11:30:56 +0200

    ci(repo): add automated PR review workflow

---
+++ .github/commitlint.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'docs',
+      'style',
+      'refactor',
+      'perf',
+      'test',
+      'build',
+      'ci',
+      'chore',
+      'revert'
+    ]],
+    'scope-enum': [2, 'always', [
+      'repo',
+      'deps',
+      'core',
+      'ui',
+      'api',
+      'docs',
+      'test',
+      'security',
+      'config'
+    ]],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always']
+  }
+};
